d4356944c9c95da70cbf6204fe58dbc27451050d
Morau Fiodar <MorauFiodar@yahoo.com>
2020-04-12 16:10:24 +0300

    feat: SquareDecomposer decomposeNumber: (Exercise 2)

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -1,8 +1,43 @@
 #import "SquareDecomposer.h"
 
 @implementation SquareDecomposer
+
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    NSNumber *n = [NSNumber numberWithLongLong:[number longLongValue]];
+    NSNumber *remain = [NSNumber numberWithLongLong:[number longLongValue] * [number longLongValue]];
+    
+    NSMutableArray *decomposeArray = [self decomposer:n remain:remain];
+    
+    if (decomposeArray == nil) {
+        return nil;
+    } else {
+        [decomposeArray removeLastObject];
+        return decomposeArray;
+    }
 }
+
+- (NSMutableArray *)decomposer:(NSNumber *)n remain:(NSNumber *)remain {
+    if ([remain longLongValue] == 0) {
+        return [NSMutableArray arrayWithObject:n];
+    }
+    
+    for(long long i = [n longLongValue] - 1; i > 0; i--) {
+        long long iSquare = i * i;
+        
+        if(([remain longLongValue] - iSquare) >= 0){
+            NSNumber *nextN = [NSNumber numberWithLongLong:i];
+            NSNumber *nextRemain = [NSNumber numberWithLongLong:([remain longLongValue] - iSquare)];
+            
+            NSMutableArray *r = [self decomposer:nextN remain:nextRemain];
+            
+            if (r != nil){
+                [r addObject:n];
+                return r;
+            }
+        }
+    }
+    
+    return nil;
+}
+
 @end
